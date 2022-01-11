c7b9fda9a8e75f61d9c08c27d32166f24ac0cd7b
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-01-11 09:59:43 -0700

    got rid of routes for now

--- app/router.js
+++ app/router.js
@@ -1,15 +1,18 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'reg-ex-rex/config/environment';
-
+import IndexRoute from './routes/index';
+import Route from '@ember/routing/route';
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
 Router.map(function () {
-  this.route('about');
-  this.route('regex', function () {
-    this.route('game');
-  });
+  this.route('index', { path: '/' });
+  // this.route('about');
+  // this.route('regex', function () {
+  //   this.route('game');
+  // });
   this.route('not-found', { path: '/*path' });
 });
+
