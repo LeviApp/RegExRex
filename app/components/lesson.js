af4cf5ee0d0fd37dfe8ce04fa2aa47b3c3a1c209
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-01-13 19:11:40 -0700

    changed the data structure for each lesson

--- app/components/lesson.js
+++ app/components/lesson.js
@@ -8,54 +8,47 @@ export default class LessonComponent extends Component {
 
     @tracked
     lesson = {
-        1: ["Lesson 1: Beginnings", "Welcome to the RegEx Rex lessons. RegEx is a way to search for patterns in text. You can search for letters, words, and digits. You can search for a certain length of words and a lot of other patterns. In normal searches, what is being used are literal characters. Searching for 'code' is searching for the exact pattern of letters that spell out the word. In the first couple of lessons, we will be focusing on literal characters to explore different patterns that RegEx can be used for. The first pattern we will discuss is the beginning. RegEx can be used to search for a pattern at the very start of a text block. Using literal characters, your first challenge is to search for the first word in this paragraph."],
-        2: ["Lesson 2: The In-Between", "Another pattern that RegEx accounts for is, obviously, found in the middle. RegEx isn't only for patterns at the start of a text block. At any point in the middle of a text block, RegEx can search for a pattern. Roses are red and violets are blue. Using literal characters, search for the rose's color in this paragraph if you want to continue."],
-        3: ["Lesson 3: The Finish", "The next pattern that deals with position is the ending. RegEx can find a pattern at the end of a text block. It doesn't matter what is at the end of a text block. It could be letters, words, numbers, or other characters. The period and other marks are always good to consider when searching strings. This is important for the beginning, middle, or end of a text block. Using a literal pattern, find the last word in this paragraph, including punctuation!"]
+        1: ["Lesson 1: Beginnings -- Welcome to the RegEx Rex course. RegEx is a way to search for patterns in text. You can search for letters, words, and digits. You can search for a certain length of words and a lot of other patterns. In normal searches, what is being used are literal characters. Searching for 'code' is searching for the exact pattern of letters that spell out the word. In the first couple of lessons, we will be focusing on literal characters to explore different patterns that RegEx can be used for. The first pattern we will discuss is the beginning. RegEx can be used to search for a pattern at the very start of a text block. Use the search bar above to solve the lesson's challenge. Using literal characters, search for the first word in the following paragraph.", "Websites are built using three main languages: HTML, CSS, and JavaScript. These three languages are usually explained by comparing them to a house. HTML is the physical brick of the house. It is the physical content which makes up the website. CSS is the paint you apply to the brick or door. It makes the content on the webpage look better. JavaScript is the doorbell of the house. Its responsibility is to produce some kind of functionality to the house. JavaScript does the same with the website.", "Websites"],
+        2: ["Lesson 2: The In-Between -- Another pattern that RegEx accounts for is, obviously, found in the middle. RegEx isn't only for patterns at the start of a text block. At any point in the middle of a text block, RegEx can search for a pattern. Roses are red and violets are blue. Using literal characters, search for the word in the following paragraph that best describes a jpeg if you want to continue.", "There are two main aspects to websites. The Front End is the visual side of the webpage. It includes elements such as the paragraph, image, and color. The Front End is very important to get right. It must be laid out correctly and visually appealing. It must be user-friendly. The Back End involves all of the data that a user might need. When a user needs to see an email, the Back End works behind the scenes to get that information. It includes apis, servers, routes, and automation.", "image"],
+        3: ["Lesson 3: The Finish -- The next pattern that deals with position is the ending. RegEx can find a pattern at the end of a text block. It doesn't matter what is at the end of a text block. It could be letters, words, numbers, or other characters. Punctuation is always good to consider when searching strings. This is important for the beginning, middle, or end of a text block. Using a literal pattern, find the last word in the following paragraph, including punctuation.", "While all websites will ultimately be using HTML, CSS, and JavaScript, this does not mean developers have not implemented technologies, called frameworks, to help them build websites. Frameworks can be used to build the Front End. Every framework has foundational code it implements under the hood. The three most popular frameworks for the Front End are React, Angular, and Vue. All three of these frameworks are based in the JavaScript programming language!", "language!"]
     }
 
-    @tracked
-    solution = {
-        1: ["Welcome"],
-        2: ["red"],
-        3: ["punctuation!"]
-    }
+    // @tracked
+    // solution = {
+    //     1: ["Welcome"],
+    //     2: ["red"],
+    //     3: ["punctuation!"]
+    // }
 
     @tracked
     answer = this.lesson[this.index]
 
-    @tracked lessonpara = this.lesson[this.index]
-    @tracked lessonparaSolid = this.lesson[this.index]
-
+    @tracked lessonexplain = this.lesson[this.index][0]
+    @tracked lessonpara = this.lesson[this.index][1]
+    @tracked lessonparaSolid = this.lesson[this.index][1]
+    @tracked solution = this.lesson[this.index][2]
     @tracked rInput = '';
     @tracked rVal = '';
     @tracked correct = false;
 
     @action
     regexFind() {
-        this.lessonpara = this.lesson[this.index]
-        this.answer = this.solution[this.index]
+        this.lessonexplain = this.lesson[this.index][0]
+        this.lessonpara = this.lesson[this.index][1]
+        this.lessonparaSolid = this.lesson[this.index][1]
+        this.solution = this.lesson[this.index][2]
 
-        this.lessonparaSolid = this.lesson[this.index]
-        let val1 = this.lessonpara[0]
-        let val2 = this.lessonpara[1]
-        let val3 = this.answer[0]
-
-        console.log(val3)
         this.rInput = document.getElementsByClassName('rInput')[0].value;
         this.rVal = new RegExp(this.rInput, 'g');
-        this.lessonpara = []
-        this.lessonpara.push(val1, val2.replace(this.rVal, `<span>$&</span>`))
+        this.lessonpara = this.lessonpara.replace(this.rVal, `<span>$&</span>`)
 
-        console.log(this.lessonpara, "This is it!")
-        console.log(val1, val2)
-        if (val2.replace(this.rVal, `<span>$&</span>`) === val2.replace(val3, `<span>$&</span>`)) {
+        if (this.lessonparaSolid.replace(this.rVal, `<span>$&</span>`) === this.lessonparaSolid.replace(this.solution, `<span>$&</span>`)) {
             this.index = this.index+1
             this.correct = true
         }
 
         else {
         this.correct = false
-        console.log("testing false")
         }
     }
 
@@ -64,11 +57,14 @@ export default class LessonComponent extends Component {
         document.getElementsByClassName('rInput')[0].value = "";
         this.correct = false;
         if (this.index <= Object.keys(this.lesson).length) {
-            this.lessonpara = this.lesson[this.index]
-            this.lessonparaSolid = this.lesson[this.index]
+            this.lessonexplain = this.lesson[this.index][0]
+            this.lessonpara = this.lesson[this.index][1]
+            this.lessonparaSolid = this.lesson[this.index][1]
+            this.solution = this.lesson[this.index][2]    
         }
             else {
-                this.lessonpara = ["CONGRATULATIONS! You finished RegEx Rex's course."]
+                this.lessonexplain = ""
+                this.lessonpara = "CONGRATULATIONS! You finished RegEx Rex's course."
             }
     }
 }
