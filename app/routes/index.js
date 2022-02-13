68d5d13c455a7c08ba49ca252f5eacc952bf45b7
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-02-13 15:05:01 -0700

    getting data using ember-data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+  value = true;
+
+  // beforeModel() {
+  //     if (this.value) {
+  //         return this.transitionTo("lesson")
+  //     }
+  // }
+
+  model() {
+    console.log('testing the index model');
+  }
+}
--- app/routes/lesson.js
+++ app/routes/lesson.js
@@ -1,6 +1,18 @@
 import Route from '@ember/routing/route';
 import { tracked } from '@glimmer/tracking';
-
+import Ember from 'ember';
+import { inject as service } from '@ember/service';
 export default class LessonRoute extends Route {
-  @tracked routeVal = false;
+  @service store;
+  index;
+  async model(params) {
+    let val = await this.store.findAll("lesson");
+    console.log("testing the posts", this.store.findAll("lesson"))
+    return val;
+  }
+
+  active() {
+    console.log('testing the lesson model with activate function');
+    console.log(localStorage.getItem('lessonI'), 'does it exist 2');
+  }
 }
